b358fe8662435edd4ac574f82e67ea1bc963d189
akaradag841-dotcom <akaradag841@gmail.com>
2026-08-03 01:21:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,7 @@
-const scene = document.getElementById("invitationScene");
-const button = document.getElementById("openButton");
-const envelope = document.querySelector(".envelope");
+const envelope = document.getElementById("envelope");
+const button = document.getElementById("openBtn");
 
-function openInvitation() {
-    scene.classList.add("opened");
-
-    // Tekrar açılmasını engelle
-    button.disabled = true;
-    envelope.style.pointerEvents = "none";
-}
-
-button.addEventListener("click", openInvitation);
-envelope.addEventListener("click", openInvitation);
-
-// Sayfa açılırken yumuşak görünme efekti
-window.addEventListener("load", () => {
-    document.body.animate(
-        [
-            { opacity: 0 },
-            { opacity: 1 }
-        ],
-        {
-            duration: 800,
-            fill: "forwards"
-        }
-    );
+button.addEventListener("click", () => {
+    envelope.classList.add("opened");
+    button.style.display = "none";
 });
